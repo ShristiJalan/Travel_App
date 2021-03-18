d30235820f81b346276e75950e630f84361ef254
ShristiJalan <shristijalan1010@gmail.com>
2021-03-19 00:46:44 +0530

    css update

--- travel_app/src/components/HeroSection.js
+++ travel_app/src/components/HeroSection.js
@@ -4,6 +4,11 @@ function HeroSection() {
     return (
         <div className='hero-container'>
             <video src="/videos/video-2.mp4" autoplay loop muted />
+            <h1>ADVENTURE AWAITS</h1>
+            <p>What are you waiting for ?</p>
+            <div className="hero-btns">
+                <Button className="btns" buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
+            </div>
         </div>
     )
 }
