ab5b8f3048a5f2e1bbd26b953bf9ae0d9ec36211
ShristiJalan <shristijalan1010@gmail.com>
2021-03-19 23:46:49 +0530

    hero section update

--- travel_app/src/components/HeroSection.js
+++ travel_app/src/components/HeroSection.js
@@ -1,4 +1,8 @@
 import React from 'react'
+import '../App.css'
+import { Button } from './Button';
+import './HeroSection.css';
+
 
 function HeroSection() {
     return (
@@ -8,6 +12,8 @@ function HeroSection() {
             <p>What are you waiting for ?</p>
             <div className="hero-btns">
                 <Button className="btns" buttonStyle='btn--outline' buttonSize='btn--large'>GET STARTED</Button>
+
+                <Button className="btns" buttonStyle='btn--primary' buttonSize='btn--large'>GET STARTED</Button>
             </div>
         </div>
     )
