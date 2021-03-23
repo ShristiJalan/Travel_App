d1cd755da35ede53688c9face5bbcf7222c56090
ShristiJalan <shristijalan1010@gmail.com>
2021-03-23 23:11:39 +0530

    image added

--- travel_app/src/components/Cards.js
+++ travel_app/src/components/Cards.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import CardItem from './CardItem'
 import './Cards.css';
+import images from "../images/img-9.jpg"
+import images1 from "../images/img-2.jpg"
 
 function Cards() {
     return (
@@ -10,7 +12,14 @@ function Cards() {
                 <div className="cards__wrapper">
                     <ul className="cards__items">
                         <CardItem
-                        src="./images/img-9.jpg"
+                        src={images}
+                        text = "Explore the hidden waterfall deep inside the Amazon Jungle"
+                        label='Adventure'
+                        path='/services'
+                        />
+
+                        <CardItem
+                        src={images1}
                         text = "Explore the hidden waterfall deep inside the Amazon Jungle"
                         label='Adventure'
                         path='/services'
